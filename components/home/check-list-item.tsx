f6e7e2bda37cd08558a1cc740334ad6da2239dbc
Kyle Rummens <k.d.rummens@gmail.com>
2023-10-01 22:47:44 -0600

    Change color of check circles

--- components/home/check-list-item.tsx
+++ components/home/check-list-item.tsx
@@ -3,7 +3,7 @@ import { Check } from "lucide-react";
 const CheckListItem = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex items-center gap-2">
-      <div className="flex h-6 w-6 items-center justify-center rounded-full bg-peach-400 text-white">
+      <div className="flex h-6 w-6 items-center justify-center rounded-full bg-blue-950 text-white">
         <Check size={14} strokeWidth={4} />
       </div>
       <div>{children}</div>
