1241f8fed29a39fcf5c883ddc3141f1aa71e80cb
Kyle Rummens <k.d.rummens@gmail.com>
2023-10-01 22:38:51 -0600

    Add security, compliance, and basic features

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Card from "@/components/home/card";
 import Balancer from "react-wrap-balancer";
 import WaitlistForm from "@/components/home/waitlist-form";
+import CheckListItem from "@/components/home/check-list-item";
 
 export default async function Home() {
   return (
     <>
-      <div className="z-10 w-full max-w-4xl px-5 xl:px-0">
+      <div className="z-10 mb-20 w-full max-w-4xl px-5 xl:px-0">
         <div className="mx-auto mb-5 flex max-w-fit animate-fade-up cursor-default items-center justify-center space-x-2 overflow-hidden rounded-full border border-peach-200 bg-peach-50 px-6 py-1">
           <p className="text-sm font-semibold text-peach-500">
             Launching soon!
@@ -36,7 +37,16 @@ export default async function Home() {
           <WaitlistForm />
         </div>
       </div>
-      <div className="mb-10 mt-20 grid w-full max-w-screen-lg animate-fade-up grid-cols-1 gap-5 px-5 md:grid-cols-2 xl:px-0">
+      <div
+        className="mb-6 w-full max-w-5xl animate-fade-up px-5 opacity-0 xl:px-0"
+        style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
+      >
+        <div className="text-2xl text-gray-700">Account Features</div>
+        <div className="text-lg text-gray-500">
+          What makes Sediment different?
+        </div>
+      </div>
+      <div className="mb-20 grid w-full max-w-screen-lg animate-fade-up grid-cols-1 gap-5 px-5 md:grid-cols-2 xl:px-0">
         <Card>
           <div className="mb-3 text-lg font-medium text-gray-700">
             Budget folders in your checking account
@@ -52,7 +62,12 @@ export default async function Home() {
           <div className="mb-3 text-lg font-medium text-gray-700">
             Smart debit card automatically enforces your budget
           </div>
-          <div className="text-gray-500">With every transaction you make with your debit card, the transaction amount is automatically withdrawn from the appropriate budget folder within your account. You can even have fine-tuned control over which budget a transaction pulls from.</div>
+          <div className="text-gray-500">
+            With every transaction you make with your debit card, the
+            transaction amount is automatically withdrawn from the appropriate
+            budget folder within your account. You can even have fine-tuned
+            control over which budget a transaction pulls from.
+          </div>
         </Card>
         <Card>
           <div className="mb-3 text-lg font-medium text-gray-700">
@@ -62,8 +77,8 @@ export default async function Home() {
             Make the most out of every purchase by leveraging our transaction
             round-up feature. It automatically rounds each transaction to the
             nearest dollar, directing the spare change straight into your
-            dedicated savings account. It&apos;s like a digital piggy bank that fills
-            itself.
+            dedicated savings account. It&apos;s like a digital piggy bank that
+            fills itself.
           </div>
         </Card>
         <Card>
@@ -74,6 +89,43 @@ export default async function Home() {
             Set unyielding spending limits for specific budgets and experience
             peace of mind like never before. Enabled budget folders ensure that
             a transaction will be declined if it would exceed your budget.
+          </div>
+        </Card>
+      </div>
+      <div
+        className="mb-6 w-full max-w-5xl animate-fade-up px-5 opacity-0 xl:px-0"
+        style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
+      >
+        <div className="text-2xl text-gray-700">Bread and Butter</div>
+        <div className="text-lg text-gray-500">
+          It's not a bank account without these features
+        </div>
+      </div>
+      <div className="mb-20 grid w-full max-w-screen-lg animate-fade-up grid-cols-1 gap-5 px-5 md:grid-cols-2 xl:px-0">
+        <Card>
+          <div className="flex flex-col gap-3">
+            <CheckListItem>Individual or joint account</CheckListItem>
+            <CheckListItem>Access to 55,000+ free ATMs</CheckListItem>
+            <CheckListItem>Physical and virtual debit cards</CheckListItem>
+            <CheckListItem>Mobile deposits</CheckListItem>
+            <CheckListItem>ACH, wire, and book payments</CheckListItem>
+          </div>
+        </Card>
+      </div>
+      <div
+        className="mb-6 w-full max-w-5xl animate-fade-up px-5 opacity-0 xl:px-0"
+        style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
+      >
+        <div className="text-2xl text-gray-700">Security and Compliance</div>
+        <div className="text-lg text-gray-500">We've got your back</div>
+      </div>
+      <div className="mb-20 grid w-full max-w-screen-lg animate-fade-up grid-cols-1 gap-5 px-5 md:grid-cols-2 xl:px-0">
+        <Card>
+          <div className="flex flex-col gap-3">
+            <CheckListItem>FDIC insured</CheckListItem>
+            <CheckListItem>BSA/AML</CheckListItem>
+            <CheckListItem>Fraud monitoring</CheckListItem>
+            <CheckListItem>Data encryption</CheckListItem>
           </div>
         </Card>
       </div>
---
+++ components/home/check-list-item.tsx
@@ -0,0 +1,14 @@
+import { Check } from "lucide-react";
+
+const CheckListItem = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex items-center gap-2">
+      <div className="flex h-6 w-6 items-center justify-center rounded-full bg-peach-400 text-white">
+        <Check size={14} strokeWidth={4} />
+      </div>
+      <div>{children}</div>
+    </div>
+  );
+};
+
+export default CheckListItem;
